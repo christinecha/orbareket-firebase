bc64a4ea49fee003e4bdc5b1f5fe7801d3f603f9
Christine Cha <hello@christinecha.com>
2016-02-10 00:32:24 -0500

    Added features: editing shows, deleting shows, displaying notices

--- admin.js
+++ admin.js
@@ -18,11 +18,13 @@ $('.add-new-show').on('click', (e) => {
   e.preventDefault()
   console.log('hi')
   ref.child('shows').push({
-    title: '',
+    title: null,
     starttime: 1456266600,
     endtime: 1456266600,
-    venue: '',
+    venue: null,
     artists: [{ artist: 'Or Bareket', instrument: 'bass' }]
+  }, () => {
+    $('.notice').html('new show added')
   })
 })
 
@@ -30,6 +32,7 @@ $('.shows').on('submit', '.update-show', (e) => {
   e.preventDefault()
 
   let show_id = $(e.target).attr('data-key')
+  console.log(show_id)
   let show_title = $(e.target).children('.new-show__title').first().val()
   let show_starttime = toUnix($(e.target).children('.new-show__starttime').first().val())
   let show_endtime = toUnix($(e.target).children('.new-show__endtime').first().val())
@@ -51,78 +54,110 @@ $('.shows').on('submit', '.update-show', (e) => {
     endtime: show_endtime,
     venue: show_venue,
     artists: artists
+  }, () => {
+    $('.notice').html('show updated')
   })
 })
 
-$('.new-show__starttime').on('keyup', (e) => {
+$('.shows').on('click', '.delete-show', (e) => {
+  e.preventDefault()
+
+  let show_id = $(e.target).parent().attr('data-key')
+  ref.child('shows').child(show_id).remove(() => {
+    $('.notice').html('show removed')
+  })
+})
+
+$('.shows').on('keyup', '.new-show__starttime', (e) => {
   let starttime = $(e.target).val()
-  console.log(starttime)
-  $('.new-show__endtime').val(starttime)
+  let endtime = $(e.target).siblings('.new-show__endtime').first().val()
+  if (starttime > endtime) {
+    $(e.target).siblings('.new-show__endtime').first().val(starttime)
+  }
 })
 
 $('.shows').on('click', '.add-artist-group', (e) => {
-  $(e.target).parent().children('.new-show__artists').first().append($('.new-show__artist-group').first().clone())
+  let $group = $('<div>')
+    .addClass('new-show__artist-group')
+  let $artist = $('<input>')
+    .addClass('new-show__artist')
+    .attr('placeholder', 'artist')
+    .attr('type', 'text')
+  let $instrument = $('<input>')
+    .addClass('new-show__instrument')
+    .attr('placeholder', 'instrument')
+    .attr('type', 'text')
+  $group.append($artist, $instrument)
+  $(e.target).parent().children('.new-show__artists').first().append($group)
 })
 
 let currentDate = new Date()
 currentDate = toUnix(currentDate)
 
-ref.child('shows').orderByChild('starttime').startAt(currentDate).on('child_added', (snapshot) => {
-  let show = snapshot.val()
+ref.child('shows').orderByChild('starttime').startAt(currentDate).on('value', (snapshot) => {
+  $('.shows').empty()
 
-  let $show_title = $('<input>')
-    .addClass('new-show__title')
-    .val(show.title)
-    .attr('type', 'text')
+  snapshot.forEach((childSnapshot) => {
+    let show = childSnapshot.val()
 
-  let $show_starttime = $('<input>')
-    .addClass('new-show__starttime')
-    .val(toLocal(show.starttime))
-    .attr('type', 'datetime-local')
+    let $show_title = $('<input>')
+      .addClass('new-show__title')
+      .val(show.title)
+      .attr('placeholder', 'show title')
+      .attr('type', 'text')
 
-  let $show_endtime = $('<input>')
-    .addClass('new-show__endtime')
-    .val(toLocal(show.endtime))
-    .attr('type', 'datetime-local')
+    let $show_starttime = $('<input>')
+      .addClass('new-show__starttime')
+      .val(toLocal(show.starttime))
+      .attr('type', 'datetime-local')
 
-  let $show_venue = $('<input>')
-    .addClass('new-show__venue')
-    .val(show.venue)
-    .attr('type', 'text')
+    let $show_endtime = $('<input>')
+      .addClass('new-show__endtime')
+      .val(toLocal(show.endtime))
+      .attr('type', 'datetime-local')
 
-  let $show_artists = $('<div>').addClass('new-show__artists')
+    let $show_venue = $('<input>')
+      .addClass('new-show__venue')
+      .val(show.venue)
+      .attr('placeholder', 'venue')
+      .attr('type', 'text')
 
-  if (show.artists) {
-    $show_artists.append(
-      show.artists.map((data, i) => {
-        let $group = $('<div>')
-          .addClass('new-show__artist-group')
-        let $artist = $('<input>')
-          .addClass('new-show__artist')
-          .val(data.artist)
-          .attr('type', 'text')
-        let $instrument = $('<input>')
-          .addClass('new-show__instrument')
-          .val(data.instrument)
-          .attr('type', 'text')
-        $group.append($artist, $instrument)
-        return $group
-      })
-    )
-  }
+    let $show_artists = $('<div>').addClass('new-show__artists')
 
-  let $show = $('<form>')
-    .addClass('update-show')
-    .attr('data-key', snapshot.key())
-    .append(
-      $show_title,
-      $show_starttime,
-      $show_endtime,
-      $show_venue,
-      $show_artists,
-      '<div class="add-artist-group">+ add another</div>',
-      '<br><button>UPDATE SHOW</button>'
-    )
+    if (show.artists) {
+      $show_artists.append(
+        show.artists.map((data, i) => {
+          let $group = $('<div>')
+            .addClass('new-show__artist-group')
+          let $artist = $('<input>')
+            .addClass('new-show__artist')
+            .val(data.artist)
+            .attr('type', 'text')
+          let $instrument = $('<input>')
+            .addClass('new-show__instrument')
+            .val(data.instrument)
+            .attr('type', 'text')
+          $group.append($artist, $instrument)
+          return $group
+        })
+      )
+    }
 
-  $('.shows').append($show)
+    let $show = $('<form>')
+      .addClass('update-show')
+      .attr('data-key', childSnapshot.key())
+      .append(
+        $show_title,
+        $show_starttime,
+        $show_endtime,
+        $show_venue,
+        $show_artists,
+        '<div class="add-artist-group">+ add another</div>',
+        '<br><button>UPDATE SHOW</button>',
+        '<br><button class="delete-show">DELETE SHOW</button>'
+      )
+
+    $('.shows').append($show)
+  })
+
 })
